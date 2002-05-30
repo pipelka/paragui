39e86cfeee13825eb98d70b69f5d05cd3efe81e3
braindead <braindead>
2002-05-30 18:27:08 +0000

    added missing DECLSPEC to pgwidgetlistex.h moved releasenotes and visualc.html to the "doc" dir

--- include/pgwidgetlistex.h
+++ include/pgwidgetlistex.h
@@ -20,9 +20,9 @@
     pipelka@teleweb.at
  
     Last Update:      $Author: braindead $
-    Update Date:      $Date: 2002/04/15 13:35:35 $
+    Update Date:      $Date: 2002/05/30 18:27:08 $
     Source File:      $Source: /sources/paragui/paragui/include/pgwidgetlistex.h,v $
-    CVS/RCS Revision: $Revision: 1.3 $
+    CVS/RCS Revision: $Revision: 1.3.6.1 $
     Status:           $State: Exp $
 */
 
@@ -40,7 +40,7 @@
 	through in a smaller 'portal' with scrollbars.
 */
 
-class PG_WidgetListEx : public PG_WidgetList {
+class DECLSPEC PG_WidgetListEx : public PG_WidgetList {
 public:
 	/**
 	Constructor of the PG_WidgetListEx class
