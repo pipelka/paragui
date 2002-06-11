ef39c39683b9d581ec468d2bebfdc6b9477c60ef
braindead <braindead>
2002-06-11 14:13:47 +0000

    fix for locating zipped theme files

--- src/themes/themeloader.cpp
+++ src/themes/themeloader.cpp
@@ -20,9 +20,9 @@
     pipelka@teleweb.at
  
     Last Update:      $Author: braindead $
-    Update Date:      $Date: 2002/05/29 15:34:09 $
+    Update Date:      $Date: 2002/06/11 14:13:47 $
     Source File:      $Source: /sources/paragui/paragui/src/themes/themeloader.cpp,v $
-    CVS/RCS Revision: $Revision: 1.3.6.1 $
+    CVS/RCS Revision: $Revision: 1.3.6.2 $
     Status:           $State: Exp $
 */
 
@@ -416,7 +416,13 @@ PG_Theme* PG_Theme::Load(const char* xmltheme) {
 	// and add it to the searchpath
 	if(info.archive.Exists(filename.c_str())) {
 		const char* path = info.archive.GetRealDir(filename.c_str());
-		std::string fullpath = (std::string)path + filename;
+		char sep = PG_FileArchive::GetDirSeparator()[0];
+		
+		std::string fullpath = (std::string)path;
+		if(fullpath[fullpath.size()-1] != sep) {
+			fullpath += sep;
+		}
+		fullpath += filename;
 
 		bool rc = info.archive.AddArchive(fullpath.c_str());
 		if(rc) {
