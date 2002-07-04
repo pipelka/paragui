8689661246c3cf73f62aa799b6451ff6e8cfdf32
braindead <braindead>
2002-07-04 09:36:50 +0000

    fixed sampleapp framework

--- sdk/sampleapp/src/sampleapp.cpp
+++ sdk/sampleapp/src/sampleapp.cpp
@@ -3,9 +3,9 @@
 
 void main() {
     PG_Application app;
+    app.LoadTheme("default");
     
     app.InitScreen(640, 480, 0);
-    app.LoadTheme("default");
     app.SetEmergencyQuit(true);
     
     app.Run();
