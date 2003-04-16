3199583efbeb44c2c8f449f3bbe60307e09dfa58
braindead <braindead>
2003-04-16 14:56:51 +0000

    cleaned up shutdown code

--- src/core/pgapplication.cpp
+++ src/core/pgapplication.cpp
@@ -20,9 +20,9 @@
     pipelka@teleweb.at
  
     Last Update:      $Author: braindead $
-    Update Date:      $Date: 2003/04/16 14:02:49 $
+    Update Date:      $Date: 2003/04/16 14:56:51 $
     Source File:      $Source: /sources/paragui/paragui/src/core/pgapplication.cpp,v $
-    CVS/RCS Revision: $Revision: 1.2.4.19 $
+    CVS/RCS Revision: $Revision: 1.2.4.20 $
     Status:           $State: Exp $
 */
 
@@ -687,14 +687,11 @@ void PG_Application::Shutdown() {
 
 	while(list != objectList.end()) {
 
-		if(*list == this || *list == NULL) {
-			list++;
-			continue;
+		if(!(*list == this || *list == NULL)) {
+			delete *list;
 		}
 
-		delete *list;
 		objectList.erase(list);
-		
 		list = objectList.begin();
 	}
 
