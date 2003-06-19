38ccf3ea0088793112cc973bbe51882f294c75fd
braindead <braindead>
2003-06-19 11:34:32 +0000

    check if buttonless windows are working

--- test/windowresize.cpp
+++ test/windowresize.cpp
@@ -10,10 +10,8 @@ int main( int argc, char **argv )
 
    app.InitScreen( 640, 480 );
 
-   PG_Window a( NULL, PG_Rect( 10, 10, 100, 100 ),
-"Window 98, the most reliable window yet." );
-   PG_Window b( NULL, PG_Rect( 60, 110, 100, 100 ),
-"Window XP, ." );
+   PG_Window a( NULL, PG_Rect( 10, 10, 100, 100 ), "Window 98, the most reliable window yet.", 0);
+   PG_Window b( NULL, PG_Rect( 60, 110, 100, 100 ), "Window XP, .", 0);
 
    a.SizeWidget( 400, 200 );
 
