9f768914a77d0373e8824c66e83d97ed9ddd8692
braindead <braindead>
2004-03-10 15:38:29 +0000

    - added copyright header

--- include/pgsingleton.h
+++ include/pgsingleton.h
@@ -1,3 +1,31 @@
+/*
+    ParaGUI - crossplatform widgetset
+    Copyright (C) 2000-2004  Alexander Pipelka
+ 
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+ 
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+ 
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+    Alexander Pipelka
+    pipelka@teleweb.at
+ 
+    Last Update:      $Author: braindead $
+    Update Date:      $Date: 2004/03/10 15:38:29 $
+    Source File:      $Source: /sources/paragui/paragui/include/pgsingleton.h,v $
+    CVS/RCS Revision: $Revision: 1.3.2.2 $
+    Status:           $State: Exp $
+*/
+
 #ifndef PG_SINGLETON_H
 #define PG_SINGLETON_H
 
