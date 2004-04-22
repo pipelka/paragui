d38b64feef91d110ea034d3a3b053544efa80333
braindead <braindead>
2004-04-22 06:27:07 +0000

    - doc update from Fred

--- include/pgthemewidget.h
+++ include/pgthemewidget.h
@@ -20,9 +20,9 @@
     pipelka@teleweb.at
 
     Last Update:      $Author: braindead $
-    Update Date:      $Date: 2004/04/17 20:39:07 $
+    Update Date:      $Date: 2004/04/22 06:27:07 $
     Source File:      $Source: /sources/paragui/paragui/include/pgthemewidget.h,v $
-    CVS/RCS Revision: $Revision: 1.3.6.4.2.9 $
+    CVS/RCS Revision: $Revision: 1.3.6.4.2.10 $
     Status:           $State: Exp $
 */
 
@@ -205,6 +205,7 @@ public:
 	/**
 	Set the fransparency of the widget
 	@param t	transparency value (0 - opaque / 255 - fully transparent)
+	@param bRecursive	if child widget's transparency should be set the same as widget
 	*/
 	void SetTransparency(Uint8 t, bool bRecursive = false);
 
