94492677fb1945476bcfe963fcb9a0fdb2b9eed5
braindead <braindead>
2004-06-26 08:05:22 +0000

    - added missing files

---
+++ include/pgstring.h
@@ -0,0 +1,45 @@
+/*
+    ParaGUI - crossplatform widgetset
+    Copyright (C) 2000,2001,2002  Alexander Pipelka
+ 
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+ 
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+ 
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+    Alexander Pipelka
+    pipelka@teleweb.at
+ 
+    Last Update:      $Author: braindead $
+    Update Date:      $Date: 2004/06/26 08:05:22 $
+    Source File:      $Source: /sources/paragui/paragui/include/Attic/pgstring.h,v $
+    CVS/RCS Revision: $Revision: 1.1.2.1 $
+    Status:           $State: Exp $
+*/
+
+#ifndef PG_STRING_H
+#define PG_STRING_H
+
+#include "paragui.h"
+
+#if defined(ENABLE_UNICODE)
+#include "ychar.h"
+#include "ystring.h"
+#define PG_Char YChar
+#define PG_String YString
+#else
+#include <string>
+#define PG_Char char
+#define PG_String std::string
+#endif
+
+#endif	// PG_STRING_H
---
+++ include/ychar.h
@@ -0,0 +1,82 @@
+/************************************************************************
+ * $Id: ychar.h,v 1.1.2.1 2004/06/26 08:05:22 braindead Exp $
+ *
+ * ------------
+ * Description:
+ * ------------
+ * ychar.h
+ *
+ * A unicode lightweight character class
+ *
+ * (C) Copyright 2004 Arabeyes, Mohammed Yousif
+ *
+ * -----------------
+ * Revision Details:    (Updated by Revision Control System)
+ * -----------------
+ *  $Date: 2004/06/26 08:05:22 $
+ *  $Author: braindead $
+ *  $Revision: 1.1.2.1 $
+ *  $Source: /sources/paragui/paragui/include/Attic/ychar.h,v $
+ *
+ *  (www.arabeyes.org - under GPL License)
+ *
+ ************************************************************************/
+
+#ifndef YCHAR_H
+#define YCHAR_H
+
+#include <stdexcept>
+#include <string>
+
+typedef unsigned int uint32;
+typedef unsigned char byte;
+
+class YChar {
+public:
+  YChar();
+  YChar(const uint32);
+  YChar(const int);
+  YChar(const char);
+  YChar(const char *) throw(std::domain_error);
+  YChar(const std::string) throw(std::domain_error);
+  
+  YChar lower() const;
+  YChar upper() const;
+  YChar title() const;
+  int digitValue() const throw(std::domain_error);
+  int hexDigitValue() const throw(std::domain_error);
+  
+  bool isNull() const;
+  bool isAsciiLetter() const;
+    
+  int bytes() const;
+  std::string utf8() const;
+  
+  static int getNumberOfContinuingOctents(byte)  throw(std::domain_error);
+  static YChar fromUtf8(const char *) throw(std::domain_error);
+  static YChar fromUtf8(const std::string) throw(std::domain_error);
+  
+  //operator char();
+  operator uint32();
+  
+  friend bool operator==(const YChar &, const YChar &);
+  friend bool operator==(const YChar &, const char &);
+  friend bool operator==(const char &, const YChar &);
+  friend bool operator==(const YChar &, const int &);
+  friend bool operator==(const int &, const YChar &);
+  friend std::ostream & operator<<( std::ostream &, YChar);
+
+private:
+    uint32 ucs4;
+};
+
+bool operator==(const YChar &, const YChar &);
+bool operator==(const YChar &, const char &);
+bool operator==(const char &, const YChar &);
+bool operator==(const YChar &, const int &);
+bool operator==(const int &, const YChar &);
+
+std::ostream & operator<<( std::ostream &, YChar);
+std::istream & operator>>( std::istream &, YChar &);
+
+#endif /* YCHAR_H */
